299802410c746e470e2f188eedf22b158727a0d7
Wes Bonelli <wpb3hw@virginia.edu>
2021-05-07 08:44:32 -0400

    bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='spg-topdown-traits',
-    version='0.4.6',
+    version='0.4.7',
     description='Extract geometric traits from top-view images of plants.',
     long_description=long_description,
     long_description_content_type="text/markdown",
