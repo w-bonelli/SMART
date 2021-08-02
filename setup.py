0a2456df9d3ab531d32ad53910d394034ed03e8c
Wes Bonelli <wpb3hw@virginia.edu>
2021-08-02 17:18:58 -0400

    debug CI

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='spg-topdown-traits',
-    version='0.4.7',
+    name='spg',
+    version='0.4.8',
     description='Extract geometric traits from top-view images of plants.',
     long_description=long_description,
     long_description_content_type="text/markdown",
