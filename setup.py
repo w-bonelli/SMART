7cf7cee23c596e27ec0f56cf1947167bfc0f4da8
Wes Bonelli <wpb3hw@virginia.edu>
2021-08-28 10:57:40 -0400

    bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='smart-arabidopsis-traits',
-    version='0.5.1',
+    version='0.5.2',
     description='Extract geometric traits from top-view images of plants.',
     long_description=long_description,
     long_description_content_type="text/markdown",
