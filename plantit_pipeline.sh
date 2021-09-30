c30aef40aa567ddba156ec25681808603c3f407f
Wes Bonelli <wpb3hw@virginia.edu>
2021-09-30 19:38:05 -0400

    update plantit pipeline and config to use -m and -l flags

--- plantit_pipeline.sh
+++ plantit_pipeline.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 python3 /opt/smart/core/color_seg.py -p $INPUT
-python3 /opt/smart/core/trait_extract_parallel_ori.py -p $INPUT
+python3 /opt/smart/core/trait_extract_parallel_ori.py -p $INPUT -m $MULTIPLE -l
